974df0ca5ef86afbb5a8885960cbba85536d268c
Villanueva-JustinDexter-BSIT32E2 <157948413+Villanueva-JustinDexter-BSIT32E2@users.noreply.github.com>
2024-04-15 11:46:51 +0800

    Done(Save and Edit)

--- Case Study/Enrollment_System/frmManageUsers.vb
+++ Case Study/Enrollment_System/frmManageUsers.vb
@@ -154,7 +154,5 @@ Public Class frmManageUsers
         End If
     End Sub
 
-    Private Sub Guna2Button2_Click(sender As Object, e As EventArgs) Handles Guna2Button2.Click
-        frmRegister.ShowDialog()
-    End Sub
+
 End Class
